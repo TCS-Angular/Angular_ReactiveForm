b8618c60d3d007ed6e3e64524b64c408ac002b73
ngactive99 <ngactive99@gmail.com>
2022-04-19 16:58:18 +0530

    radio button adn validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,10 +13,28 @@ import {
 })
 export class AppComponent {
   title = 'reactiveForms';
-  // form = new FormGroup({
-  //   firstname: new FormControl('', Validators.required),
-  //   password: new FormControl('', Validators.required),
-  // });
-
-  constructor(formbuilder: FormBuilder) {}
+  contactForm;
+  constructor( private formbuilder: FormBuilder) {
+    this.contactForm= this.formbuilder.group({
+      firstname:["", [Validators.required, Validators.minLength(5)]],
+      email:["", [Validators.required, Validators.email]],
+      gender:["", [Validators.required]]
+    })
+  }
+  get firstname() {
+     return this.contactForm.get('firstname');
+  }
+  get email(){
+    return this.contactForm.get("email")
+  }
+  get gender() {
+    return this.contactForm.get("gender");
+  }
+  onSubmit() {
+    console.log(this.contactForm.value);
+    
+  }
+  reset(){
+    this.contactForm.reset()
+  }
 }
